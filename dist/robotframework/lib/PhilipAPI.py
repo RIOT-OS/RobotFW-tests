5760f8bab2d5b5a87dc15d9b3a63484a6add6630
smlng <s@mlng.net>
2019-02-11 21:26:56 +0100

    add periph_uart test

--- dist/robotframework/lib/PhilipAPI.py
+++ dist/robotframework/lib/PhilipAPI.py
@@ -1,6 +1,9 @@
+import serial
+
 from riot_pal import LLMemMapIf, PHILIP_MEM_MAP_PATH
 from robot.version import get_version
 from time import sleep
+
 
 class PhilipAPI(LLMemMapIf):
 
@@ -16,6 +19,38 @@ class PhilipAPI(LLMemMapIf):
         ret.append(self.execute_changes())
         sleep(1)
         ret.append(self.write_reg('sys.cr', 0x00))
+        ret.append(self.execute_changes())
+        sleep(1)
+        return ret
+
+    def setup_uart(self, mode=0, baudrate=115200,
+                   parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+                   rts=True):
+        '''Setup tester's UART.'''
+        ret = list()
+        ret.append(self.write_reg('uart.mode', int(mode)))
+
+        ret.append(self.write_reg('uart.baud', int(baudrate)))
+
+        # setup UART control register
+        ctrl = 0
+        if parity == serial.PARITY_EVEN:
+            ctrl = ctrl | 0x02
+        elif parity == serial.PARITY_ODD:
+            ctrl = ctrl | 0x04
+
+        if stopbits == serial.STOPBITS_TWO:
+            ctrl = ctrl | 0x01
+        # invert RTS level as it is a low active signal
+        if not rts:
+            ctrl = ctrl | 0x08
+
+        ret.append(self.write_reg('uart.ctrl', ctrl))
+
+        # reset status register
+        ret.append(self.write_reg('uart.status', 0x00))
+
+        # apply changes
         ret.append(self.execute_changes())
         sleep(1)
         return ret
---
+++ tests/periph_uart/tests/periph_uart_if.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2018 Kevin Weiss, for HAW Hamburg  <kevin.weiss@haw-hamburg.de>
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+"""@package PyToAPI
+This module handles parsing of information from RIOT periph_uart test.
+"""
+from riot_pal import DutShell
+
+
+class PeriphUartIf(DutShell):
+    """Interface to the node with periph_uart firmware."""
+    FW_ID = 'periph_uart'
+    DEFAULT_DEV = 1
+    DEFAULT_BAUD = 115200
+
+    def uart_init(self, dev=DEFAULT_DEV, baud=DEFAULT_BAUD):
+        """Initialize DUT's UART."""
+        return self.send_cmd("init {} {}".format(dev, baud))
+
+    def uart_mode(self, data_bits, parity, stop_bits, dev=DEFAULT_DEV):
+        """Setup databits, parity and stopbits."""
+        return self.send_cmd(
+            "mode {} {} {} {}".format(dev, data_bits, parity, stop_bits))
+
+    def uart_send_string(self, test_string, dev=DEFAULT_DEV):
+        """Send data via DUT's UART."""
+        return self.send_cmd("send {} {}".format(dev, test_string))
+
+    def uart_get_id(self):
+        """Get the id of the fw."""
+        return self.send_cmd('uart_get_id')
