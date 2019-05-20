ae78e097b8a6fd6bf9b29f5b8d2755b3d725eb75
MrKevinWeiss <weiss.kevin604@gmail.com>
2019-05-20 12:05:02 +0200

    refactor(philip): Update tests to run philip release 1.0.0
    This changes the tests to work with the first release of philip
    It uses philip pal instead of riot pal
    It also updates some UART tests since the protocol changed

--- dist/robotframework/lib/PhilipAPI.py
+++ dist/robotframework/lib/PhilipAPI.py
@@ -1,56 +1,41 @@
 import serial
 
-from riot_pal import LLMemMapIf, PHILIP_MEM_MAP_PATH
+from philip_pal import Phil
 from robot.version import get_version
-from time import sleep
 
 
-class PhilipAPI(LLMemMapIf):
-
+class PhilipAPI(Phil):
+    """Robot framework wrapper for PHiLIP"""
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
     ROBOT_LIBRARY_VERSION = get_version()
 
     def __init__(self, port, baudrate):
-        super(PhilipAPI, self).__init__(PHILIP_MEM_MAP_PATH, 'serial', port, baudrate)
-
-    def reset_dut(self):
-        ret = list()
-        ret.append(self.write_reg('sys.cr', 0xff))
-        ret.append(self.execute_changes())
-        sleep(1)
-        ret.append(self.write_reg('sys.cr', 0x00))
-        ret.append(self.execute_changes())
-        sleep(1)
-        return ret
+        super().__init__(port, baudrate)
 
     def setup_uart(self, mode=0, baudrate=115200,
                    parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
                    rts=True):
-        '''Setup tester's UART.'''
+        """Setup tester's UART."""
         ret = list()
-        ret.append(self.write_reg('uart.mode', int(mode)))
-
+        mode = int(mode)
+        assert mode >= 0 and mode < 3, "Invalid mode setting for the if_type"
+        ret.append(self.write_reg('uart.mode.if_type', mode))
         ret.append(self.write_reg('uart.baud', int(baudrate)))
 
         # setup UART control register
-        ctrl = 0
         if parity == serial.PARITY_EVEN:
-            ctrl = ctrl | 0x02
+            ret.append(self.write_reg('uart.mode.parity', 1))
         elif parity == serial.PARITY_ODD:
-            ctrl = ctrl | 0x04
+            ret.append(self.write_reg('uart.mode.parity', 2))
 
         if stopbits == serial.STOPBITS_TWO:
-            ctrl = ctrl | 0x01
+            ret.append(self.write_reg('uart.mode.stop_bits', 1))
         # invert RTS level as it is a low active signal
         if not rts:
-            ctrl = ctrl | 0x08
+            ret.append(self.write_reg('uart.mode.rts', 1))
 
-        ret.append(self.write_reg('uart.ctrl', ctrl))
-
-        # reset status register
-        ret.append(self.write_reg('uart.status', 0x00))
+        ret.append(self.write_reg('uart.mode.init', 0))
 
         # apply changes
         ret.append(self.execute_changes())
-        sleep(1)
         return ret
