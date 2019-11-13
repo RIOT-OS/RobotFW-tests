5af466d4bdc9e281507a86d813944de87573fe4e
MrKevinWeiss <weiss.kevin604@gmail.com>
2019-11-13 14:20:14 +0100

    dist/tools/ci: Fix script naming for print env

--- dist/tools/ci/print_environment.sh
+++ dist/tools/ci/print_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-python3 $SCRIPTPATH/print_python_versions.py
+python3 $SCRIPTPATH/print_environment.py
 bash $SCRIPTPATH/../../../RIOT/dist/tools/ci/print_toolchain_versions.sh
