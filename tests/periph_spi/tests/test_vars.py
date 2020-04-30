53c05b60c9519f4c05f9d56ade62ac8066accf99
MarcelStenzel <marcel.stenzel@haw-hamburg.de>
2020-04-30 09:03:05 +0200

    tests/periph_spi: Add tests for transfer of different amounts of bytes
    This tests the SPI transfer of 1, 2 and 16 bytes with the 4 given functions of the spi API.

--- tests/periph_spi/tests/test_vars.py
+++ tests/periph_spi/tests/test_vars.py
@@ -1,1 +1,16 @@
-LIST__VAL_1 = [254, 0, 1, 2, 3]
+def str_4():
+	result = ""
+	for i in range(3, 16 + 3):
+	    result += str(i) + " "
+	return result
+
+LIST__VAL_1 = [254]
+LIST__VAL_2 = [41]
+LIST__VAL_3 = [41, 42]
+
+VAL_4 = str_4()
+LIST__VAL_5 = [254] + list(range(3, 16 + 3 - 1))
+
+LIST__VAL_6 = [5]
+LIST__VAL_7 = [42, 43]
+LIST__VAL_8 = [5, 6, 7] + list(range(44, 16 + 44 - 3))
