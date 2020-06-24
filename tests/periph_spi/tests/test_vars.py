60c1878bd51a5bfeef9dccf90189116383b975e8
MarcelStenzel <marcel.stenzel@haw-hamburg.de>
2020-06-24 16:07:32 +0200

    periph_spi: Add tests for 1 Byte transfer
    The test uses now the byte_ticks of PHiLIP instead of the frame_ticks.
    At least for stm32 CPUs there are some problems with higher clock frequenzies.

--- tests/periph_spi/tests/test_vars.py
+++ tests/periph_spi/tests/test_vars.py
@@ -20,9 +20,24 @@ LIST__VAL_10 = [254, 11, 2, 14, 5]
 
 
 def spi_speed_limits(spi_speed, sys_clock_speed, bytes):
-	expected_byte_ticks = round(8 / spi_speed * sys_clock_speed)
-	expected_frame_ticks = expected_byte_ticks * bytes
-	error_tol = round(expected_byte_ticks + expected_byte_ticks * bytes / 8)
-	lower_limit = expected_frame_ticks - error_tol
-	upper_limit = expected_frame_ticks + error_tol
+
+	for i in range(2):
+		if i == 0:
+			spi_speed_limit = spi_speed * 1.20
+		else:
+			spi_speed_limit = spi_speed * 0.8
+
+		expected_byte_ticks = round((8 * sys_clock_speed) / spi_speed_limit)
+		expected_frame_ticks = expected_byte_ticks * bytes
+		error_tol = round(expected_byte_ticks * bytes / 8)
+
+		if i == 0:
+			lower_limit = expected_frame_ticks - error_tol
+		else:
+			upper_limit = expected_frame_ticks + error_tol
+
 	return [lower_limit, upper_limit]
+
+
+def convert_ticks_to_us(ticks, sys_clock_speed):
+	return round(ticks / sys_clock_speed * 1000000, 3)
